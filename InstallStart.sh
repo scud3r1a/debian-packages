559fdc345643d4d80fd43ddbfde1bb020a40d01e
Git OI Development <gitoi.development@gmx.ch>
2015-05-15 23:00:16 +0200

    main content

---
+++ InstallStart.sh
@@ -0,0 +1,146 @@
+#UPDATE PREPARATION
+echo
+echo ___________________________________________________
+echo 
+echo UPDATE PREPARATION
+echo ___________________________________________________
+echo 
+
+#reading repository list
+sudo apt-get update
+
+#adding some required repositories
+sudo add-apt-repository main
+sudo add-apt-repository universe
+sudo add-apt-repository multiverse
+sudo add-apt-repository restricted
+sudo add-apt-repository ppa:ehbello/fritzing --yes
+sudo add-apt-repository ppa:git-core/ppa --yes
+sudo add-apt-repository ppa:sunab/sigil-git --yes
+sudo add-apt-repository ppa:tualatrix/ppa --yes
+sudo apt-add-repository ppa:jtaylor/keepass --yes
+sudo add-apt-repository ppa:paolorotolo/android-studio --yes
+sudo add-apt-repository ppa:ubuntu-mozilla-daily/ppa --yes
+sudo add-apt-repository 'deb http://repository.spotify.com/ stable non-free' --yes
+sudo add-apt-repository 'deb http://deb.torproject.org/torproject.org utopic main' --yes
+sudo add-apt-repository 'deb-src http://deb.torproject.org/torproject.org utopic main' --yes
+
+#adding missing keys for repositories
+sudo apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 94558F59 --yes
+gpg --keyserver keys.gnupg.net --recv 886DDD89
+gpg --export A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89 | sudo apt-key add -
+
+#reading new repository list again
+sudo apt-get update 
+
+#INSTALLATION AND UPDATE
+echo
+echo ___________________________________________________
+echo
+echo INSTALLATION AND UPDATE
+echo ___________________________________________________
+echo
+
+#installing standard packages or check 
+sudo apt-get install dpkg --yes
+sudo apt-get install build-essential --yes
+sudo apt-get install ntfs-3g --yes
+sudo apt-get install aptitude --yes
+sudo apt-get install nautilus-open-terminal --yes
+sudo apt-get install preload --yes
+sudo apt-get install gparted --yes
+sudo apt-get install p7zip --yes
+	sudo apt-get install p7zip-full --yes
+	sudo apt-get install p7zip-rar --yes
+sudo apt-get install ubuntu-restricted-extras --yes
+sudo apt-get install unity-tweak-tool --yes
+
+	#python and pip plus upgrade
+	sudo apt-get install python-dev --yes
+	sudo apt-get install python-pip --yes
+	sudo pip install --upgrade pip
+	sudo pip install --upgrade virtualenv
+
+	#git with graphical user interfaces
+	sudo apt-get install git --yes
+	sudo apt-get install gitk --yes
+	sudo apt-get install git-gui --yes
+	
+	#tor with several tools
+	sudo apt-get install tor deb.torproject.org-keyring --yes
+	sudo apt-get install tor-arm --yes
+	sudo apt-get install python-stem --yes
+	sudo apt-get install vidalia --yes
+	
+	#network - ftp, ssh, vpn
+	sudo apt-get install putty --yes
+	sudo apt-get install filezilla --yes
+	sudo apt-get install openvpn --yes
+	sudo apt-get install wireshark --yes
+		sudo apt-get install tshark --yes 
+	sudo apt-get install synaptic --yes
+	sudo apt-get install samba --yes
+		sudo apt-get install samba-common-bin --yes
+		
+	#android tools
+	sudo apt-get install android-tools-adb --yes
+	sudo apt-get install android-tools-fastboot --yes
+
+#entertainment and media		
+sudo apt-get install spotify-client --yes
+sudo apt-get install vlc --yes
+
+	#content creation (media)
+	sudo apt-get install audacity --yes
+	sudo apt-get install libreoffice --yes
+	sudo apt-get install openshot --yes
+	sudo apt-get install gimp --yes
+	sudo apt-get install inkscape --yes
+	sudo apt-get install k3b --yes
+	sudo apt-get install handbrake --yes
+	sudo apt-get install musescore --yes
+	sudo apt-get install calibre --yes
+	sudo apt-get install sigil --yes
+	sudo apt-get install lyx --yes
+
+#development, science and calculation (production)
+sudo apt-get install android-studio --yes
+sudo apt-get install eclipse --yes
+sudo apt-get install octave --yes
+	sudo apt-get install octave-doc --yes 
+	sudo apt-get install octave-epstk --yes
+	sudo apt-get install gnuplot --yes
+sudo apt-get install geogebra --yes
+sudo apt-get install gelemental --yes
+sudo apt-get install avogadro --yes
+sudo apt-get install logisim --yes
+sudo apt-get install fritzing --yes
+
+#internet and mail
+sudo apt-get install thunderbird --yes
+	sudo apt-get install kleopatra --yes
+sudo apt-get install firefox --yes
+	sudo apt-get install firefox-trunk --yes
+
+#others
+sudo apt-get install keepass2 --yes
+sudo apt-get install keepassx --yes
+sudo apt-get install steam --yes
+sudo apt-get install bleachbit --yes
+sudo apt-get install virtualbox linux-headers-$(uname -r) --yes
+
+#UGRADE
+sudo add-apt-repository --remove ppa:webupd8team/tor-browser --yes
+clear
+sudo apt-get upgrade --yes
+clear
+
+echo
+echo ___________________________________________________
+echo
+echo SYSTEM AND REPOSITORIES SUCCESSFULLY UPDATED
+echo ___________________________________________________
+echo
+echo
+
+
--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # InstallStart
-The most important script for a new Ubuntu installation
+##The most important script for a new Ubuntu installation
+###(Only woking with Ubuntu and Ubuntu- or Debian-like distributions)
+
+This is a very simple script that installs the most important normal-user applications and packages
+for your Debian or Ubuntu based Linux OS like Linux Mint or Ubuntu itself.
+The script is very self-explaining.
+Feel free to add your own favourite programs for Linux.
+
+For the best experience, please use an distribution version comparable with Ubuntu 14.04 or greater.
+
+Thanks, 
+
+##Kutsubato, gitoidevelopment
+
+
+
+
