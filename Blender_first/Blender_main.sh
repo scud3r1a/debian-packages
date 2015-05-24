85f0d106601cc53656ed414428d52c6fbae14d6d
Kutsubato <gitoi.development@gmx.ch>
2015-05-24 20:54:58 +0200

    Update Blender_main.sh

--- Blender_first/Blender_main.sh
+++ Blender_first/Blender_main.sh
@@ -1,3 +1,5 @@
+echo
+clear
 wget "http://ftp.halifax.rwth-aachen.de/blender/release/Blender2.74/blender-2.74-linux-glibc211-x86_64.tar.bz2"
 sudo tar xvjf "blender-2.74-linux-glibc211-x86_64.tar.bz2"
 sudo rm "blender-2.74-linux-glibc211-x86_64.tar.bz2"
@@ -6,5 +8,5 @@ sudo mkdir /opt/blender
 sudo cp -r blender/* /opt/blender/
 sudo cp "InstallStart/Blender_first/blender.desktop" "/usr/share/applications/"
 echo
-InstallStart finished (Blender installation)
+InstallStart finished 
 echo
