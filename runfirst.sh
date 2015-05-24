81e0f6975fcf439817b3b68a952e0fdcf196979b
Kutsubato <gitoi.development@gmx.ch>
2015-05-24 15:24:07 +0200

    Update runfirst.sh

--- runfirst.sh
+++ runfirst.sh
@@ -2,4 +2,4 @@ sudo apt-get install git --yes
 sudo rm -r InstallStart 
 sudo git clone "https://github.com/gitoidevelopment/InstallStart"
 cd InstallStart/
-sudo sh Install.sh --yes
+sudo sh InstallStart.sh --yes
