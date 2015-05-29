8b70984dcab67f3a3c92bbb18b83cde239722970
Kutsubato <gitoi.development@gmx.ch>
2015-05-29 15:35:49 +0200

    Update and rename InstallStart_Blender.sh to InstallStart-Blender.sh

--- Blender/InstallStart-Blender.sh
+++ Blender/InstallStart-Blender.sh
@@ -1,9 +1,18 @@
+clear 
 echo
-clear
+
+echo
+echo "Downloading Blender (64 bit)"
+echo
+
 wget "http://ftp.halifax.rwth-aachen.de/blender/release/Blender2.74/blender-2.74-linux-glibc211-x86_64.tar.bz2"
 sudo tar xvjf "blender-2.74-linux-glibc211-x86_64.tar.bz2"
 sudo rm "blender-2.74-linux-glibc211-x86_64.tar.bz2"
 mv "blender-2.74-linux-glibc211-x86_64" "blender"
+
+echo
+echo "Installing Blender"
+echo
 
 sudo rm -r /opt/blender
 sudo mkdir /opt/blender
@@ -14,5 +23,5 @@ sudo cp "InstallStart/Blender_first/blender.desktop" "/usr/share/applications/"
 
 sudo rm -r blender
 echo
-InstallStart finished 
+echo "InstallStart (Blender) finished"
 echo
