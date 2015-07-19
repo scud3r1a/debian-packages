82f2d40990b1b12508d84806ac46c768d2a77a46
Kutsubato <gitoi.development@gmx.ch>
2015-07-19 14:32:29 +0200

    Update Configuration.sh

--- Configuration.sh
+++ Configuration.sh
@@ -2,3 +2,8 @@ echo
 echo Advanced configuration starting
 echo
 
+#onetime installations
+add-apt-repository ppa:webupd8team/tor-browser --yes
+  apt-get install tor-browser --yes
+    add-apt-repository --remove ppa:webupd8team/tor-browser --yes
+
