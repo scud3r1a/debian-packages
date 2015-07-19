03ccf606594444f8303dfcdaaf58b1ea5582e1f1
Kutsubato <gitoi.development@gmx.ch>
2015-07-19 14:14:34 +0200

    Update Start.sh

--- Start.sh
+++ Start.sh
@@ -3,16 +3,13 @@ echo
 echo Update starting
 echo
 
-cd 
-
 sudo apt-get install git --yes
 sudo rm -r InstallStart
 sudo git clone "https://github.com/gitoidevelopment/InstallStart"
-chmod +x InstallStart/
-cd InstallStart/
+chmod 666 InstallStart/
 
 echo
 
 #InstallStart
 cd
-sudo sh "InstallStart/InstallStart.sh"
+sudo sh "InstallStart/Install.sh"
