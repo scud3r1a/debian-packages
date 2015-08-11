0c6daf68a96e29390e724d78d730537de6a63d64
Kutsubato <gitoi.development@gmx.ch>
2015-08-11 19:02:55 +0200

    Update InstallStart.sh

--- InstallStart.sh
+++ InstallStart.sh
@@ -151,20 +151,6 @@ apt-get install gambas3 --yes
 apt-get install geany --yes
 echo
 
-	#virtualization
-	apt-get install docker.io --yes
-	apt-get install vagrant --yes
-	apt-get install virtualbox --yes
-	apt-get install qemu --yes
-		apt-get install qemu-system-x86 --yes
-		apt-get install qemu-system --yes
-		apt-get install qemu-kvm --yes
-			apt-get install qemu-kvm-extras --yes
-			apt-get install libvirt-bin --yes
-			apt-get install virt-manager --yes
-				apt-get install virt-viewer --yes
-				apt-get install python-spice-client-gtk --yes
-				
 	#android tools
 	apt-get install android-tools-adb --yes
 	apt-get install android-tools-fastboot --yes
@@ -227,54 +213,6 @@ clear
 
 echo
 echo InstallStart finished
-echo
-
-###############################################################################
-
-echo
-echo Docker Setup starting
-echo
-
-###############################################################################
-
-#Docker update
-wget -qO- https://get.docker.com/ | sh
-echo
-	
-	#Docker Compose installation
-	pip install -U docker-compose
-	echo
-	
-	#Docker machine installation
-	curl -L downloads.hypriot.com/docker-machine_0.4.0-dev_linux-amd64 > /usr/local/bin/docker-machine
-		chmod +x /usr/local/bin/docker-machine
-			echo
-	
-	#Docker images
-	docker pull swarm:latest
-	docker pull dockerui/dockerui
-	echo
-	docker pull centos
-	docker pull nginx
-	docker pull debian
-	docker pull ubuntu
-		docker pull ubuntu:wily
-		docker pull ubuntu-upstart
-		docker pull jujusolutions/jujubox
-			docker pull jujusolutions/charmbox 
-	docker pull rails
-	docker pull django
-	docker pull php:latest
-		docker pull php:5.6-apache
-	docker pull alpine
-	docker pull fedora
-	echo
-	clear
-
-###############################################################################
-	
-echo
-echo Docker Setup finished
 echo
 
 ###############################################################################
