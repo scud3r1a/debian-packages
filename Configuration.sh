153bd27c011b793448bdfc023ecdf918a314bedd
Kutsubato <githubato@users.noreply.github.com>
2015-10-28 12:38:59 +0100

    Update Configuration.sh

--- Configuration.sh
+++ Configuration.sh
@@ -6,8 +6,20 @@ echo
 echo Just start this script once!
 echo
 
-#Tor installation
+# Tor installation
 add-apt-repository ppa:webupd8team/tor-browser --yes
   apt-get install tor-browser --yes
     add-apt-repository --remove ppa:webupd8team/tor-browser --yes
       echo
+
+# Docker installation
+apt-key adv --keyserver hkp://pgp.mit.edu:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D && \
+touch /etc/apt/sources.list.d/docker.list && \ 
+  echo "deb https://apt.dockerproject.org/repo ubuntu-wily main" >> /etc/apt/sources.list.d/docker.list && \
+apt-get update -mqqy && \
+  apt-get purge lxc-docker* && apt-cache policy docker-engine && \
+apt-get install -y docker-engine
+
+echo
+echo "Done."
+echo
