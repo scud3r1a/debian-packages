a331b6889b92a1abd0655edc89a477ee93eee239
Kutsubato <githubato@users.noreply.github.com>
2015-10-28 12:40:45 +0100

    Update Configuration.sh

--- Configuration.sh
+++ Configuration.sh
@@ -16,7 +16,7 @@ add-apt-repository ppa:webupd8team/tor-browser --yes
 apt-key adv --keyserver hkp://pgp.mit.edu:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 touch /etc/apt/sources.list.d/docker.list 
   echo "deb https://apt.dockerproject.org/repo ubuntu-wily main" >> /etc/apt/sources.list.d/docker.list 
-apt-get update -mqqy 
+apt-get update 
   apt-get purge lxc-docker* && apt-cache policy docker-engine 
 apt-get install -y docker-engine
 
