b730b3576c8b9aaffb92f47b3476e5124b9d2d25
Kutsubato <kutsubato@web.de>
2015-12-10 21:31:06 +0100

    Correct

--- InstallStart.sh
+++ InstallStart.sh
@@ -6,6 +6,7 @@ add-apt-repository universe
 add-apt-repository multiverse
 add-apt-repository restricted
 echo
+
 add-apt-repository ppa:snappy-dev/tools --yes
 add-apt-repository ppa:graphics-drivers/ppa --yes
 add-apt-repository ppa:ehbello/fritzing --yes
@@ -17,20 +18,17 @@ add-apt-repository ppa:pipelight/stable --yes
 apt-add-repository ppa:ehoover/compholio --yes
 add-apt-repository ppa:danielrichter2007/grub-customizer --yes
 
-	# Adding missing keys for repositories
-	apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 94558F59 \
-	gpg --keyserver keys.gnupg.net --recv 886DDD89
-	gpg --export A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89 | apt-key add -
-
 #############################################################################################
 echo
+
 # Update, upgrade
 apt-get update --fix-missing
-apt-get dist-upgrade --force-yes \
-apt-get update --fix-missin
-#############################################################################################
+apt-get dist-upgrade --force-yes --yes
+apt-get update --fix-missing
 
+#############################################################################################
 echo
+
 # Packages
 apt-get install --force-yes --yes --no-install-recommends \
 		ubuntu-restricted-extras \
@@ -66,7 +64,7 @@ apt-get install --force-yes --yes --no-install-recommends \
 		binfmt-support \
 		debootstrap \
 		dosfstools \
-		apt-cacher-ng
+		apt-cacher-ng \
 		wine \
 		playonlinux \
 		filezilla \
@@ -128,24 +126,20 @@ apt-get install --force-yes --yes --no-install-recommends \
  		keepassx \
  		steam
 
+#############################################################################################
 echo
 
-#############################################################################################
-
 # Static packages
-
 rm -rf deb
 wget https://atom.io/download/deb
 	dpkg -i deb
 		rm -rf deb
 			echo
-
 rm -rf ipscan_3.3.3_amd64.deb
 wget https://github.com/angryziber/ipscan/releases/download/3.3.3/ipscan_3.3.3_amd64.deb
 	dpkg -i ipscan_3.3.3_amd64.deb
 		rm -rf ipscan_3.3.3_amd64.deb
 			echo
-
 rm -rf google-chrome-stable_current_amd64.deb
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 	dpkg -i google-chrome-stable_current_amd64.deb
