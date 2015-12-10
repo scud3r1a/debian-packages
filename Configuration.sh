883726c4e39b04cdaeb0c4417e781715e43f4f57
Kutsubato <kutsubato@web.de>
2015-12-10 20:17:26 +0100

    Update

--- Configuration.sh
+++ Configuration.sh
@@ -1,5 +1,5 @@
 echo
-echo Advanced configuration starting
+echo Advanced configuration
 echo
 echo #############################################
 echo
@@ -14,11 +14,23 @@ add-apt-repository ppa:webupd8team/tor-browser --yes
 
 # Docker installation
 apt-key adv --keyserver hkp://pgp.mit.edu:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-touch /etc/apt/sources.list.d/docker.list 
-  echo "deb https://apt.dockerproject.org/repo ubuntu-wily main" >> /etc/apt/sources.list.d/docker.list 
-apt-get update 
-  apt-get purge lxc-docker* && apt-cache policy docker-engine 
+touch /etc/apt/sources.list.d/docker.list
+echo "deb https://apt.dockerproject.org/repo ubuntu-wily main" >> /etc/apt/sources.list.d/docker.list
+apt-get update --fix-missing
+apt-get purge lxc-docker* && apt-cache policy docker-engine
 apt-get install -y docker-engine
+
+# Dotfiles
+curl https://raw.githubusercontent.com/githubato/Dotfiles/master/install.sh | sh \
+&& dotfiles host
+
+# Gosu
+curl -o /usr/local/bin/gosu -SL \
+  "https://github.com/tianon/gosu/releases/download/1.2/gosu-$(dpkg --print-architecture)" \
+&& chmod +x /usr/local/bin/gosu
+
+# easy
+curl https://raw.githubusercontent.com/githubato/easy/master/install.sh | sh
 
 echo
 echo "Done."
--- InstallStart.sh
+++ InstallStart.sh
@@ -1,190 +1,165 @@
-###############################################################################
-
-echo
-echo InstallStart starting
 echo
 
-#############################################################################################
-
-#adding required repositories
+# Adding required repositories
 add-apt-repository main
 add-apt-repository universe
 add-apt-repository multiverse
 add-apt-repository restricted
 echo
-add-apt-repository ppa:snappy-dev/tools --yes
-add-apt-repository ppa:graphics-drivers/ppa --yes
-add-apt-repository ppa:ehbello/fritzing --yes
-add-apt-repository ppa:sunab/sigil-git --yes
-add-apt-repository ppa:tualatrix/ppa --yes
-add-apt-repository ppa:paolorotolo/android-studio --yes
-apt-add-repository ppa:cordova-ubuntu/ppa --yes
-add-apt-repository ppa:pipelight/stable --yes
-apt-add-repository ppa:ehoover/compholio --yes
+add-apt-repository ppa:snappy-dev/tools \
+add-apt-repository ppa:graphics-drivers/ppa \
+add-apt-repository ppa:ehbello/fritzing \
+add-apt-repository ppa:sunab/sigil-git \
+add-apt-repository ppa:tualatrix/ppa \
+add-apt-repository ppa:paolorotolo/android-studio \
+apt-add-repository ppa:cordova-ubuntu/ppa \
+add-apt-repository ppa:pipelight/stable \
+apt-add-repository ppa:ehoover/compholio \
 
-	#adding missing keys for repositories
-	apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 94558F59 --yes
+	# Adding missing keys for repositories
+	apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 94558F59 \
 	gpg --keyserver keys.gnupg.net --recv 886DDD89
 	gpg --export A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89 | apt-key add -
 
 #############################################################################################
 echo
-#reading new repository list again
+# Update, upgrade
 apt-get update --fix-missing
-apt-get update -ymmq
+apt-get dist-upgrade --force-yes \
+apt-get update --fix-missin
 #############################################################################################
 
 echo
-#installing standard packages or check
-apt-get install --force-yes --yes \
-	tree \
-	dpkg \
-	build-essential \
-	automake \
-	gksu \
-	preload \
-	wget \
-	curl \
-	ntfs-3g \
-	hfsutils \
-	hfsprogs \
-	exfat-fuse \
-	archivemount \
-	makeself \
-	libfuse-dev \
-	apache2-utils \
-	libssl-dev \
-	which \
-	man \
-	ca-certificates \
-	vim \
-	nano \
-	python \
-	openssh-server \
-	openssh-client \
-	openvpn \
-	openssl \
-	iputils \
-	iproute2 \
-	binfmt-support \
-	debootstrap \
-	dosfstools \
-	apt-cacher-ng
+# Packages
+apt-get install --force-yes --yes --no-install-recommends \
+		tree \
+		dpkg \
+		build-essential \
+		automake \
+		gksu \
+		preload \
+		wget \
+		curl \
+		ntfs-3g \
+		hfsutils \
+		hfsprogs \
+		exfat-fuse \
+		archivemount \
+		makeself \
+		libfuse-dev \
+		apache2-utils \
+		libssl-dev \
+		which \
+		man \
+		ca-certificates \
+		vim \
+		nano \
+		python \
+		openssh-server \
+		openssh-client \
+		openvpn \
+		openssl \
+		iputils \
+		iproute2 \
+		binfmt-support \
+		debootstrap \
+		dosfstools \
+		apt-cacher-ng
+		wine \
+		playonlinux \
+		filezilla \
+		openvpn \
+		wireshark \
+		tshark \
+		synaptic \
+		privoxy \
+		samba \
+		spotify-client \
+ 		vlc \
+ 		shutter \
+		audacity \
+	 	libreoffice \
+	 	dia \
+	 	kdenlive \
+	 	gimp \
+	 	inkscape \
+	 	k3b \
+	 	handbrake \
+	 	musescore \
+	 	calibre \
+	 	sigil \
+	 	scribus \
+		scribus-template \
+	 	lyx \
+	 	pdfchain \
+		pdftk \
+ 		android-studio \
+ 		eclipse \
+ 		spyder \
+ 		gedit \
+ 		vim \
+ 		gambas2 \
+ 		emacs \
+ 		idle \
+ 		lazarus \
+ 		gtcreator \
+ 		gambas3 \
+ 		geany \
+	 	android-tools-adb \
+	 	android-tools-fastboot \
+ 		geogebra \
+ 		gelemental \
+ 		avogadro \
+ 		logisim \
+ 		fritzing \
+ 		lightspeed \
+ 		marble \
+	 	marble-qt \
+		thunderbird \
+ 		firefox \
+ 		chromium \
+	 	chromium-l10n \
+ 		chromium-browser \
+	 	chromium-browser-l10n \
+	 	chromium-codecs-ffmpeg \
+	 	chromium-codecs-ffmpeg-extra \
+ 		keepassx \
+ 		steam \
+ 		nvidia-355
 
-	apt-get install wine --yes
-	apt-get install playonlinux --yes
-
+echo
 #############################################################################################
 
-	#network and connecting
-	apt-get install filezilla --yes
-	apt-get install openvpn --yes
-	apt-get install wireshark --yes
-		apt-get install tshark --yes
-	apt-get install synaptic --yes
-	apt-get install privoxy --yes
-	apt-get install samba --yes
-	apt-get install sshpass --force-yes --yes
-
-#entertainment and media
-apt-get install spotify-client --yes
-apt-get install vlc --yes
-apt-get install shutter --yes
-
-	#content creation (media)
-	apt-get install audacity --yes
-	apt-get install libreoffice --yes
-	apt-get install dia --yes
-	apt-get install kdenlive --yes
-	apt-get install gimp --yes
-	apt-get install inkscape --yes
-	apt-get install k3b --yes
-	apt-get install handbrake --yes
-	apt-get install musescore --yes
-	apt-get install calibre --yes
-	apt-get install sigil --yes
-	apt-get install scribus --yes
-		apt-get install scribus-template --yes
-	apt-get install lyx --yes
-	apt-get install pdfchain --yes
-		apt-get install pdftk --yes
-
-#development
-apt-get install cordova-cli --yes
-apt-get install android-studio --yes
-apt-get install eclipse --yes
-apt-get install spyder --yes
-apt-get install gedit --yes
-apt-get install vim --yes
-apt-get install gambas2 --yes
-apt-get install emacs --yes
-apt-get install idle --yes
-apt-get install lazarus --yes
-apt-get install gtcreator --yes
-apt-get install gambas3 --yes
-apt-get install geany --yes
-echo
-
-	#android tools
-	apt-get install android-tools-adb --yes
-	apt-get install android-tools-fastboot --yes
-
-#science and calculation
-apt-get install geogebra --yes
-apt-get install gelemental --yes
-apt-get install avogadro --yes
-apt-get install logisim --yes
-apt-get install fritzing --yes
-apt-get install lightspeed --yes
-apt-get install marble --yes
-	apt-get install marble-qt --yes
-
-#internet and mail
-apt-get install thunderbird --yes
-apt-get install firefox --yes
-apt-get install chromium --yes
-	apt-get install chromium-l10n --yes
-apt-get install chromium-browser --yes
-	apt-get install chromium-browser-l10n --yes
-	apt-get install chromium-codecs-ffmpeg --yes
-	apt-get install chromium-codecs-ffmpeg-extra --yes
-
-#others
-apt-get install keepassx --yes
-apt-get install steam --yes
-apt-get install nvidia-355 --yes
-echo
-
-#############################################################################################
-
+rm -rf deb
 wget https://atom.io/download/deb
 	dpkg -i deb
-		rm deb
+		rm -rf deb
 			echo
-			
+
+rm -rf ipscan_3.3.3_amd64.deb
 wget https://github.com/angryziber/ipscan/releases/download/3.3.3/ipscan_3.3.3_amd64.deb
 	dpkg -i ipscan_3.3.3_amd64.deb
-		rm ipscan_3.3.3_amd64.deb
+		rm -rf ipscan_3.3.3_amd64.deb
 			echo
-			
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb 
-	dpkg -i google-chrome-stable_current_amd64.deb 
-		rm google-chrome-stable_current_amd64.deb 
+
+rm -rf google-chrome-stable_current_amd64.deb
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+	dpkg -i google-chrome-stable_current_amd64.deb
+		rm -rf google-chrome-stable_current_amd64.deb
 			echo
 
 #############################################################################################
 
-#final upgrade
+# Final upgrade
 clear
 echo
 
-apt-get upgrade --yes
-apt-get autoremove --yes
+apt-get upgrade --force-yes --yes
+apt-get autoremove --force-yes --yes
 apt-get clean
-clear
 
 echo
-echo InstallStart finished
+echo "Done."
 echo
 
 ###############################################################################
