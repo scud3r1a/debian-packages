b9ccc2c779673a13879fdd4b19dd25a78eb3b5f2
Kutsubato <kutsubato@web.de>
2015-12-10 20:21:22 +0100

    Update

--- Configuration.sh
+++ Configuration.sh
@@ -1,33 +1,33 @@
 echo
 echo Advanced configuration
 echo
-echo #############################################
-echo
-echo Just start this script once!
-echo
 
 # Tor installation
 add-apt-repository ppa:webupd8team/tor-browser --yes
-  apt-get install tor-browser --yes
-    add-apt-repository --remove ppa:webupd8team/tor-browser --yes
-      echo
+apt-get install tor-browser --force-yes --yes
+add-apt-repository --remove ppa:webupd8team/tor-browser --yes
+echo
 
 # Docker installation
 apt-key adv --keyserver hkp://pgp.mit.edu:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 touch /etc/apt/sources.list.d/docker.list
 echo "deb https://apt.dockerproject.org/repo ubuntu-wily main" >> /etc/apt/sources.list.d/docker.list
 apt-get update --fix-missing
-apt-get purge lxc-docker* && apt-cache policy docker-engine
-apt-get install -y docker-engine
+apt-get purge lxc-docker*
+apt-cache policy docker-engine
+apt-get install --force-yes docker-engine
+echo
 
 # Dotfiles
 curl https://raw.githubusercontent.com/githubato/Dotfiles/master/install.sh | sh \
 && dotfiles host
+echo
 
 # Gosu
 curl -o /usr/local/bin/gosu -SL \
   "https://github.com/tianon/gosu/releases/download/1.2/gosu-$(dpkg --print-architecture)" \
 && chmod +x /usr/local/bin/gosu
+echo
 
 # easy
 curl https://raw.githubusercontent.com/githubato/easy/master/install.sh | sh
--- README.md
+++ README.md
@@ -1,17 +1,14 @@
-#InstallStart
-##The most important script for a new Ubuntu installation
+# InstallStart
+### The most important script for a new Ubuntu installation
 
 This is a very simple script that installs the most important normal-user applications and packages
 for your Ubuntu (based) OS.
 
 ```
-apt-get install -y git && \
-  rm -f -r InstallStart && \
-
+apt-get install --yes git && \
+rm -rf InstallStart && \
 git clone "https://github.com/githubato/InstallStart" && \
-  clear && \
-    echo && \
-
+echo && \
 sh InstallStart/InstallStart.sh && \
-  sh InstallStart/Configuration.sh
+sh InstallStart/Configuration.sh
 ```
