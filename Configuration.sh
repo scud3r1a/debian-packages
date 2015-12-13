7bebc50c261407c3c64443015f4d6f3cc98e0681
Kutsubato <githubato@users.noreply.github.com>
2015-12-13 15:23:28 +0100

    Update Configuration.sh

--- Configuration.sh
+++ Configuration.sh
@@ -12,6 +12,7 @@ echo
 
 # Docker installation
 apt-key adv --keyserver hkp://pgp.mit.edu:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+rm -rf /etc/apt/sources.list.d/docker.list
 touch /etc/apt/sources.list.d/docker.list
 echo "deb https://apt.dockerproject.org/repo ubuntu-wily main" >> /etc/apt/sources.list.d/docker.list
 apt-get update --fix-missing
