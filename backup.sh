121f521cf41c7d1c14f08e8342fd2c74e6f65abb
Kutsubato <githubato@users.noreply.github.com>
2016-04-19 14:44:57 +0200

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,12 +1,8 @@
 mkdir -m 777 -p /backup
 chmod -R 777 /backup
 
-rm -rf tmpbck.txt
-date +"%D" >> tmpbck.txt
-date +"%T" >> tmpbck.txt
-
-DAY=`cat tmpbck.txt |sed -n '1p'`
-TIME=`cat tmpbck.txt |sed -n '2p'`
+DAY=`date +"%D"`
+TIME=`date +"%T"`
 
 tar -cvz -f /backup/data-$DAY-$TIME.tar.gz /data/
 tar -cvz -f /backup/vagrant-$DAY-$TIME.tar.gz /vagrant/
