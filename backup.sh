3db7bdf8332d53ed1023b84e0ea73c92f1389bed
Kutsubato <githubato@users.noreply.github.com>
2016-04-19 14:41:45 +0200

    Update and rename backup to backup.sh

--- backup.sh
+++ backup.sh
@@ -9,5 +9,7 @@ DAY=`cat tmpbck.txt |sed -n '1p'`
 TIME=`cat tmpbck.txt |sed -n '2p'`
 
 tar -cvz -f /backup/data-$DAY-$TIME.tar.gz /data/
+tar -cvz -f /backup/vagrant-$DAY-$TIME.tar.gz /vagrant/
+tar -cvz -f /backup/root-$DAY-$TIME.tar.gz /root/
 
 rm -rf tmpbck.txt
