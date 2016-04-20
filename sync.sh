4dc22b91c8dc853b5c5a6ddb31ebe00ad1e57db2
Kutsubato <githubato@users.noreply.github.com>
2016-04-20 23:08:55 +0200

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -1,3 +1,9 @@
 # sh sync.sh <IP Backup Server>
 
+chmod -R 777 /data /root /vagrant
+
 rsync -r -a -v -e "ssh -l root" --delete /data/ $1:/data/
+rsync -r -a -v -e "ssh -l root" --delete /root/ $1:/root/
+rsync -r -a -v -e "ssh -l root" --delete /vagrant/ $1:/vagrant/
+
+rsync -r -a -v -e "ssh -l root" /backup/ $1:/backup/
