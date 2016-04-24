2acd42524af235f3079ffc37522a34617c70b712
Kutsubato <githubato@users.noreply.github.com>
2016-04-24 18:04:01 +0200

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -4,9 +4,6 @@ rsync -r -a -v --rsh="sshpass -p $3 ssh -l $2" --delete /data/ $1:/data/
 rsync -r -a -v --rsh="sshpass -p $3 ssh -l $2" --delete /root/ $1:/root/
 rsync -r -a -v --rsh="sshpass -p $3 ssh -l $2" --delete /vagrant/ $1:/vagrant/
 
-rsync -r -a -v --rsh="sshpass -p $3 ssh -l $2" /backup/ $1:/backup/
-rsync -r -a -v --rsh="sshpass -p $3 ssh -l $2" $1:/backup/ /backup/
-
 echo
 echo "Done."
 echo
