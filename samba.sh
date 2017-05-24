23365e18472de309ee03aa9f83ae806141f52e8f
Kutsubato <githubato@users.noreply.github.com>
2017-05-24 22:32:14 +0200

    Update samba.sh

--- samba.sh
+++ samba.sh
@@ -3,6 +3,12 @@ echo
 apt-get update --fix-missing
 apt-get install --force-yes --yes samba samba-common-bin
 
+mkdir -m 777 -p /media/Volume0
+mkdir -m 777 -p /media/Volume1
+mkdir -m 777 -p /media/Volume2
+mkdir -m 777 -p /media/Volume3
+mkdir -m 777 -p /media/Volume4
+
 echo
 echo "New user (Samba): root"
 echo
@@ -13,3 +19,5 @@ echo
 cp -avr smb.conf /etc/samba/smb.conf
 echo
 /etc/init.d/samba restart
+
+
