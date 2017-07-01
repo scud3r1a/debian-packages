1e33e85fe014f54e42caada857d2bcd6b4249353
Kutsubato <githubato@users.noreply.github.com>
2017-07-01 19:27:07 +0200

    Update install-openvc.sh

--- install-openvc.sh
+++ install-openvc.sh
@@ -9,6 +9,7 @@ apt-get install -y \
 build-essential \
 cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev \
 python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev \
+libgtk2.0-dev pkg-config \
 wget
 
 # Pull sources
@@ -40,5 +41,5 @@ make -j4
 # Installation
 make install
 
-.. cd
+cd ..
 popd
